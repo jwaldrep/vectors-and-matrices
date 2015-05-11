1ac0890e4a2f85e43e0a84cebcfb841fc611feea
John Waldrep <johnwaldrep@gmail.com>
2015-05-11 13:37:12 -0400

    Add function prototypes

--- matrix_math.py
+++ matrix_math.py
@@ -1,2 +1,34 @@
 class ShapeException(Exception):
     pass
+
+
+def shape(vector):
+    """Take a vector or matrix and return a tuple with the
+    number of rows (for a vector) or the number of rows and columns
+    (for a matrix.)"""
+    pass
+
+def matrix_vector_multiply(x, y):
+    pass
+def matrix_matrix_multiply(x, y):
+    pass
+def matrix_scalar_multiply(x, y):
+    pass
+def matrix_col(x, y):
+    pass
+def matrix_row(x, y):
+    pass
+def magnitude(x):
+    pass
+def vector_mean(x, y):
+    pass
+def vector_multiply(x, y):
+    pass
+def dot(x, y):
+    pass
+def vector_sum(x, y):
+    pass
+def vector_sub(x, y):
+    pass
+def vector_add(x, y):
+    pass
