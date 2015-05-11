1d968455d7260dabac8181253dd471606d330efa
John Waldrep <johnwaldrep@gmail.com>
2015-05-11 14:02:10 -0400

    Implement vector_add() with checking for ShapeException; 5/21 tests pass

--- matrix_math.py
+++ matrix_math.py
@@ -11,36 +11,34 @@ def shape(array):
     except TypeError:
         return (len(array),)
 
-    """
-    m = [3, 4]
-    v = [1, 3, 0]
-
-    assert shape(m) == (2,)
-    assert shape(v) == (3,)
-    assert shape([1]) == (1,)
-    """
-
-def matrix_vector_multiply(x, y):
-    pass
-def matrix_matrix_multiply(x, y):
-    pass
-def matrix_scalar_multiply(x, y):
-    pass
-def matrix_col(x, y):
-    pass
-def matrix_row(x, y):
-    pass
-def magnitude(x):
-    pass
-def vector_mean(x, y):
-    pass
-def vector_multiply(x, y):
-    pass
-def dot(x, y):
+def vector_add(x, y):
+    if shape(x) != shape(y):
+        raise ShapeException
+    try:
+        return [x_val + y[idx_r][idx_c]
+                for idx_r, row in enumerate(x)
+                for idx_c, x_val in row]
+    except TypeError:
+        return [x_val + y[idx_r] for idx_r, x_val in enumerate(x)]
+def vector_sub(x, y):
     pass
 def vector_sum(x, y):
     pass
-def vector_sub(x, y):
+def dot(x, y):
     pass
-def vector_add(x, y):
+def vector_multiply(x, y):
+    pass
+def vector_mean(x, y):
+    pass
+def magnitude(x):
+    pass
+def matrix_row(x, y):
+    pass
+def matrix_col(x, y):
+    pass
+def matrix_scalar_multiply(x, y):
+    pass
+def matrix_matrix_multiply(x, y):
+    pass
+def matrix_vector_multiply(x, y):
     pass
